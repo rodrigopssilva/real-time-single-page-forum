e27b6a1172183534093ce3df5ca1400caccf0c23
Rodrigo Silva <rodrigo.pssilva@gmail.com>
2019-10-11 16:36:39 -0300

    JWT authentication and Middleware
    Adding the JWT authentication system and the JWT middleware.

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -10,6 +10,16 @@ use Symfony\Component\HttpFoundation\Response;
 class LikeController extends Controller
 {
     /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('JWT');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
