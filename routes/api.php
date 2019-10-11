29bbd1035442e2caa5ea10b8db51aec9f28b8181
Rodrigo Silva <rodrigo.pssilva@gmail.com>
2019-10-11 14:56:39 -0300

    CRUD for Reply
    Implementing CRUD for Reply.

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use \Illuminate\Support\Facades\Route;
 
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
+Route::apiResource('/question/{question}/reply', 'ReplyController');
